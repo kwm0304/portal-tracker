16419fb391f0c8c28e37af5e34f86d904b2f562c
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-01-23 18:32:40 -0500

    changed update to add oldSchool flag to db entries

--- server/update.js
+++ server/update.js
@@ -1,56 +1,45 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+const fs = require("fs").promises;
 
 const update = async () => {
   const client = new MongoClient(process.env.MONGO_URI);
 
   try {
+    const year = "2022";
+    const path = `../ncaa/data/ncaaf/stats/${year}/ncaaf_transfers_${year}.json`;
+    const transfers = JSON.parse(await fs.readFile(path, "utf8"));
+
     await client.connect();
     console.log("Connected to MongoDB");
 
     const db = client.db("transfer_players");
     const collection = db.collection("players");
 
-    const filter = {
-      "playerInfo.firstName": "Warren",
-      "playerInfo.lastName": "Thompson",
-      "playerInfo.school": "Arkansas",
-    };
+    for (const transfer of transfers) {
+      const filter = {
+        "playerInfo.firstName": transfer.firstName,
+        "playerInfo.lastName": transfer.lastName,
+        "playerInfo.year": year,
+        "playerInfo.school": transfer.newSchool,
+      };
 
-    const updateDoc = {
-      $push: {
-        stats: {
-          g: "13",
-          rec: "19",
-          rec_yds: "304",
-          rec_yds_per_rec: "16",
-          rec_td: "2",
-          scrim_att: "19",
-          scrim_yds: "304",
-          scrim_yds_per_att: "16",
-          scrim_td: "2",
-          pass_cmp: "1",
-          pass_att: "2",
-          pass_yds: "27",
-          pass_cmp_pct: "50",
-          pass_yds_per_att: "13.5",
-          adj_pass_yds_per_att: "-9",
-          pass_int: "1",
-          pass_rating: "63.4",
-          school_name: "Arkansas",
+      const updateDoc = {
+        $set: {
+          "playerInfo.oldSchool": transfer.oldSchool,
         },
-      },
-    };
+      };
 
-    const result = await collection.updateOne(filter, updateDoc, { new: true });
-    console.log(
-      `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
-    );
+      const result = await collection.updateOne(filter, updateDoc);
+      console.log(
+        `For player ${transfer.firstName} ${transfer.lastName}, matched ${result.matchedCount}, updated: ${result.modifiedCount}`
+      );
+    }
   } catch (error) {
     console.error("Error updating:", error);
   } finally {
     await client.close();
-    console.log("MongoDB connection closed");
+    console.log("Disconnected from MongoDB");
   }
 };
 
